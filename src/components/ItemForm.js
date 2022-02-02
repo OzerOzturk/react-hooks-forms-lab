0e6c62ae180db4c9b8f022d721ca48244af2a4d3
OzerOzturk <ozerozturk1636@gmail.com>
2022-02-02 16:44:47 +0300

    forms lab adjust by fix itemform and shoppinglist components.

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("Produce");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onItemFormSubmit({ id: uuid(), name, category });
+  }
+
+  function handleChangeItem(e) {
+    setName(e.target.value);
+  }
+
+  function handleChangeCategory(e) {
+    setCategory(e.target.value);
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChangeItem}
+        />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select
+          name="category"
+          value={category}
+          onChange={handleChangeCategory}
+        >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
